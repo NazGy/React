59a4430792396e745c4f3a591fd835b073067d24
NazGy <nazargy@gmail.com>
2020-05-19 00:15:54 -0400

    Additional Styling to HomePage

--- my_app/src/component/HomePage.js
+++ my_app/src/component/HomePage.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Typography from '@material-ui/core/Typography';
-import {withRouter} from 'react-router';
+import { withRouter } from 'react-router';
 
 class HomePage extends React.Component {
     render() {
         return (
-            <div>
-                <Typography>Hello I'm the Home Page</Typography>
-
+            <div className={"center_page"}>
+                <div className={"row"}>
+                    <img className={"photo"} style={{ width: '400', height: '400', borderRadius: '50%' }} src={require('../pictures/Home_Photo.jpg')} />
+                    <Typography className={"name"}>Nazar Georgis-Yap.</Typography>
+                </div>
+                <div className={"row"}>
+                    <Typography className={"title"}>Software Developer</Typography>
+                </div>
             </div>
         )
     }
