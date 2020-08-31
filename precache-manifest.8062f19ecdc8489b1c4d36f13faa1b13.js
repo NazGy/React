594b496dd0183e98d2a8d9a66caa2793fc474576
NazGy <nazargy@gmail.com>
2020-08-31 00:27:45 -0400

    Updates

---
+++ precache-manifest.8062f19ecdc8489b1c4d36f13faa1b13.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb4d392269c91a11235f6867ebb0fe11",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "dc917fb3315b17a329f0",
+    "url": "/React/static/css/main.883b4c65.chunk.css"
+  },
+  {
+    "revision": "b28caa9c60d323eeb954",
+    "url": "/React/static/js/2.955471c1.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/React/static/js/2.955471c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc917fb3315b17a329f0",
+    "url": "/React/static/js/main.9883f09b.chunk.js"
+  },
+  {
+    "revision": "4478c4d596606f97dd88",
+    "url": "/React/static/js/runtime-main.9e711646.js"
+  },
+  {
+    "revision": "69a22bedab3fe2e9d12c2613633467a7",
+    "url": "/React/static/media/AP_Polygon_1.69a22bed.png"
+  },
+  {
+    "revision": "992f9d8c7c8932ad11c9a6524d7ba43f",
+    "url": "/React/static/media/AP_Polygon_2.992f9d8c.png"
+  },
+  {
+    "revision": "9213c5efdf2bfdc5111edc5a3a51ea77",
+    "url": "/React/static/media/AP_Polygon_3.9213c5ef.png"
+  },
+  {
+    "revision": "d54af619e7b9089d8f8548d67d6d9b75",
+    "url": "/React/static/media/AP_Polygon_4.d54af619.png"
+  },
+  {
+    "revision": "5884eb031ed0fb38b44c597cd45fad1b",
+    "url": "/React/static/media/AP_Polygon_5.5884eb03.png"
+  },
+  {
+    "revision": "06f911b0112b54b3ccff0919deaf0bb4",
+    "url": "/React/static/media/BalanceCheck_Logo.06f911b0.png"
+  },
+  {
+    "revision": "5180ab4b6a006fc0a03e9fa5bc081bbf",
+    "url": "/React/static/media/Cover.5180ab4b.png"
+  },
+  {
+    "revision": "f960b3319aa03bed49a643b7f630408c",
+    "url": "/React/static/media/ElementScanner_Logo.f960b331.png"
+  },
+  {
+    "revision": "58568ca9bc3c76a94487ef2dc694a894",
+    "url": "/React/static/media/Facialy_Logo.58568ca9.png"
+  },
+  {
+    "revision": "c8ed30c6b4718e4d2b6c120e8b8195c9",
+    "url": "/React/static/media/GLG.c8ed30c6.png"
+  },
+  {
+    "revision": "1ed78f421324858cc5bf0b050d4c2ab0",
+    "url": "/React/static/media/Home_Photo.1ed78f42.jpg"
+  },
+  {
+    "revision": "2d0410af863bee09c2b55462b8f69d13",
+    "url": "/React/static/media/MyInterviewer_Logo.2d0410af.png"
+  },
+  {
+    "revision": "a79297583816df8b6ee980bc09c47a69",
+    "url": "/React/static/media/Radar_Logo.a7929758.png"
+  },
+  {
+    "revision": "d6463837aa1040121adc65c2786c5961",
+    "url": "/React/static/media/git.d6463837.png"
+  }
+]);
